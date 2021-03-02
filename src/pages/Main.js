8fafa7c262f3df640b7f68f74405fd71a208d43c
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-03-02 10:04:49 -0300

    update @react-navigation version

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 
 function Main() {
   return (
-    <>
-      <Text>
-        Oi! Seus arquivos das duas páginas estão na pasta ./src/pages. Você pode
-        utilizar o react navigation para navegar entre as telas :)
-      </Text>
-      <Text>
-        Hi! Your files for both pages are in the ./src/pages folder. You can use
-        react navigation to navigate between screens :)
-      </Text>
-    </>
+    <Text>
+      Hi! Your files for both pages are in the ./src/pages folder. You can use
+      react navigation to navigate between screens :) hehe
+    </Text>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Main from './pages/Main';
 import Exercise from './pages/Exercise';
 
-const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    Exercise,
-  }),
-);
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Exercise" component={Exercise} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default Routes;
